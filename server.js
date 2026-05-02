4ea2f34ce0e586c97789a36ebd3626cbcc8a467d
A4S3-Next <aarav.kshah@nextschool.org>
2026-05-02 20:17:39 +0530

    Initialize Express server with OpenAI integration (Java Script)

---
+++ server.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const OpenAI = require("openai");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const client = new OpenAI({
+  apiKey: process.env.GROQ_API_KEY,
+  baseURL: "https://api.groq.com/openai/v1"
+});
+
+app.post("/chat", async (req, res) => {
+
+  try {
+
+    const userMessage =
+      req.body.message;
+
+    const response =
+      await client.chat.completions.create({
+
+         model: "llama-3.3-70b-versatile",
+
+        messages: [
+
+          {
+            role: "system",
+            content:
+              "You are a kind, supportive assistant helping teenagers with stress, bullying, emotions, and school problems."
+          },
+
+          {
+            role: "user",
+            content: userMessage
+          }
+
+        ]
+
+      });
+
+    res.json({
+      reply:
+        response.choices[0].message.content
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      reply:
+        "I'm here for you."
+    });
+
+  }
+
+});
+
+app.listen(3000, () => {
+
+  console.log(
+    "Server running on http://localhost:3000"
+  );
+
+});
